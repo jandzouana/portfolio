95664a6a38a3ff8d5236c442a33f40918bba5ada
Jessica Andzouana <jandzouana@gmail.com>
2017-11-20 20:43:44 -0800

    Fixed preloader

--- scripts/preloader.js
+++ scripts/preloader.js
@@ -1,34 +1,62 @@
-var totalTimeToLoad = 0;
-function OnImageLoaded () {
-  //alert ("The image has been loaded in " + this.src);
-}
-
-function OnImageFailed (img) {
-  alert ("The image has not been loaded: " + this.src);
-}
-
-/*
-TRY THIS
-var preloadImage = function (url) {
-    try {
-        var _img = new Image();
-        _img.src = url;
-    } catch (e) { }
-}
-*/
-
 var images = [];
 (function(){
   function id(v){ return document.getElementById(v); }
-  function preload() {
-      for (var i = 0; i < arguments.length; i++) {
-          var startTime = new Date().getTime();
-          images[i] = new Image();
-          images[i].onload = OnImageLoaded;
-          images[i].onerror = OnImageFailed;
-          images[i].src = preload.arguments[i];
-      }
+  function loadbar() {
+    var ovrl = id("overlay"),
+        prog = id("progress"),
+        stat = id("progstat"),
+        img = document.images,
+        c = 0,
+        regimgtot = img.length,
+        tot = regimgtot + arguments.length;
+    //alert ("Total images: " + tot);
+    if(tot == 0) return doneLoading();
+
+    function imgLoaded(){
+      c += 1;
+      var perc = ((100/tot*c) << 0) +"%";
+      prog.style.width = perc;
+      stat.innerHTML = "Loading "+ perc;
+      //alert(this.src + " succesfully loaded");
+      if(c === tot) return doneLoading();
+    }
+    function imgFailed(){
+      alert(this.src + " failed to load");
+    }
+    function doneLoading(){
+      ovrl.style.opacity = 0;
+      setTimeout(function(){
+        ovrl.style.display = "none";
+      }, 1200);
+    }
+    for(var j = 0; j < regimgtot; j++) {
+      var tImg     = new Image();
+      tImg.onload  = imgLoaded;
+      tImg.onerror = imgFailed;
+      tImg.src     = img[j].src;
+    }
+    for (var i = 0; i < arguments.length; i++) {
+        images[i] = new Image();
+        images[i].onload = imgLoaded;
+        images[i].onerror = imgFailed;
+        images[i].src = loadbar.arguments[i];
+    }
   }
+  document.addEventListener('DOMContentLoaded', loadbar(
+      "/jessica/portfolio/pictures/characters/1med.jpg",
+      "/jessica/portfolio/pictures/characters/2med.jpg",
+      "/jessica/portfolio/pictures/characters/3med.jpg",
+      "/jessica/portfolio/pictures/characters/4med.jpg",
+      "/jessica/portfolio/pictures/characters/5med.jpg",
+      "/jessica/portfolio/pictures/characters/6med.jpg",
+      "/jessica/portfolio/pictures/characters/7med.jpg",
+      "/jessica/portfolio/pictures/characters/8med.jpg",
+      "/jessica/portfolio/pictures/characters/9med.jpg",
+      "/jessica/portfolio/pictures/characters/10med.jpg"
+  ), false);
+}());
+
+/*
   document.addEventListener('DOMContentLoaded', preload(
       "/pictures/characters/1med.jpg",
       "/pictures/characters/2med.jpg",
@@ -52,27 +80,4 @@ var images = [];
       "/pictures/characters/10sm.jpg"
   ), false);
 }());
-
-
-
-function addLoadEvent(func) {
-	var oldonload = window.onload;
-	if (typeof window.onload != 'function') {
-		window.onload = func;
-	} else {
-		window.onload = function() {
-			if (oldonload) {
-				oldonload();
-			}
-			func();
-		}
-	}
-}
-
-/*
-addLoadEvent(preload(
-    "/pictures/characters/1med.jpg",
-    "/pictures/characters/2med.jpg",
-    "/pictures/characters/3med.jpg",
-    "/pictures/characters/10med.jpg"
-));
+*/
--- scripts/tests.js
+++ scripts/tests.js
@@ -42,3 +42,84 @@ alert(el);
 var url = el.css("background-image");
 alert(url);
 */
+
+
+// Preload (works)
+var totalTimeToLoad = 0;
+function OnImageLoaded () {
+  //alert ("The image has been loaded in " + this.src);
+}
+
+function OnImageFailed (img) {
+  alert ("The image has not been loaded: " + this.src);
+}
+
+/*
+TRY THIS
+var preloadImage = function (url) {
+    try {
+        var _img = new Image();
+        _img.src = url;
+    } catch (e) { }
+}
+*/
+
+var images = [];
+(function(){
+  function id(v){ return document.getElementById(v); }
+  function preload() {
+      for (var i = 0; i < arguments.length; i++) {
+          var startTime = new Date().getTime();
+          images[i] = new Image();
+          images[i].onload = OnImageLoaded;
+          images[i].onerror = OnImageFailed;
+          images[i].src = preload.arguments[i];
+      }
+  }
+  document.addEventListener('DOMContentLoaded', preload(
+      "/pictures/characters/1med.jpg",
+      "/pictures/characters/2med.jpg",
+      "/pictures/characters/3med.jpg",
+      "/pictures/characters/4med.jpg",
+      "/pictures/characters/5med.jpg",
+      "/pictures/characters/6med.jpg",
+      "/pictures/characters/7med.jpg",
+      "/pictures/characters/8med.jpg",
+      "/pictures/characters/9med.jpg",
+      "/pictures/characters/10med.jpg",
+      "/pictures/characters/1sm.jpg",
+      "/pictures/characters/2sm.jpg",
+      "/pictures/characters/3sm.jpg",
+      "/pictures/characters/4sm.jpg",
+      "/pictures/characters/5sm.jpg",
+      "/pictures/characters/6sm.jpg",
+      "/pictures/characters/7sm.jpg",
+      "/pictures/characters/8sm.jpg",
+      "/pictures/characters/9sm.jpg",
+      "/pictures/characters/10sm.jpg"
+  ), false);
+}());
+
+
+
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function() {
+			if (oldonload) {
+				oldonload();
+			}
+			func();
+		}
+	}
+}
+
+/*
+addLoadEvent(preload(
+    "/pictures/characters/1med.jpg",
+    "/pictures/characters/2med.jpg",
+    "/pictures/characters/3med.jpg",
+    "/pictures/characters/10med.jpg"
+));
